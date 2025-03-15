38716e6e3fbd0bd0fac2e2baff6f31c2af90cc62
Dario Quispe <106852390+Minkaspr@users.noreply.github.com>
2025-03-14 22:39:14 -0500

    chore: update environment durations

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   CACHE_EXPIRATION_HOURS: 12,
-  API_CACHE_EXPIRATION_HOURS: 2,
+  API_CACHE_EXPIRATION_HOURS: 4,
   API_URL: "https://dev-highlights-be.vercel.app/api/v1",
   API_DOMAIN: "http://localhost:3000",
   API_PREFIX: "/api/v1"
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   CACHE_EXPIRATION_HOURS: 24,
-  API_CACHE_EXPIRATION_HOURS: 12,
+  API_CACHE_EXPIRATION_HOURS: 18,
   API_URL: "https://dev-highlights-be.vercel.app/api/v1",
   API_DOMAIN: "https://dev-highlights-be.vercel.app",
   API_PREFIX: "/api/v1"
